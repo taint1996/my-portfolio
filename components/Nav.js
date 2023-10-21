1e53915ebe414e3e66c79ea222526d11221d579b
Tai Nguyen-Trung (Teth Adam) <tai.nguyentrung1996@gmail.com>
2023-10-22 00:14:50 +0700

    Adding Layout UI

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,51 @@
 // icons
 import {
-  HiHome,
-  HiUser,
-  HiViewColumns,
-  HiRectangleGroup,
-  HiChatBubbleBottomCenterText,
-  HiEnvelope,
-} from 'react-icons/hi2';
+	HiHome,
+	HiUser,
+	HiViewColumns,
+	HiRectangleGroup,
+	HiChatBubbleBottomCenterText,
+	HiEnvelope,
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
-  {
-    name: 'testimonials',
-    path: '/testimonials',
-    icon: <HiChatBubbleBottomCenterText />,
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-    icon: <HiEnvelope />,
-  },
+	{ name: "home", path: "/", icon: <HiHome /> },
+	{ name: "about", path: "/about", icon: <HiUser /> },
+	{ name: "services", path: "/services", icon: <HiRectangleGroup /> },
+	{ name: "work", path: "/work", icon: <HiViewColumns /> },
+	{
+		name: "testimonials",
+		path: "/testimonials",
+		icon: <HiChatBubbleBottomCenterText />,
+	},
+	{
+		name: "contact",
+		path: "/contact",
+		icon: <HiEnvelope />,
+	},
 ];
 
+// Next Link
+import Link from "next/link";
+
+// Next Router
+import { useRouter } from "next/router";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+	return (
+		<nav>
+			<div>
+				{navData.map((link, linkIdx) => {
+					return (
+						<Link href={link.path} key={linkIdx}>
+							{link.icon}
+						</Link>
+					);
+				})}
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
