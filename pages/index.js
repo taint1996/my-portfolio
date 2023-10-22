28ffb32ff69ad9f61dcf41a1cc496b9db910d72c
Tai Nguyen-Trung (Teth Adam) <tai.nguyentrung1996@gmail.com>
2023-10-22 22:14:29 +0700

    Done Transition - Nav - TopLeftImg. On doing Image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+// next image
+import Image from "next/image";
+
+import ParticlesConatainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
 const Home = () => {
-  return <div>Home</div>;
+	return <div>Home</div>;
 };
 
 export default Home;
