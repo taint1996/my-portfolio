b73f90a4a0c960fadf8b378551aa0fa3e8f0524a
Tai Nguyen-Trung (Teth Adam) <tai.nguyentrung1996@gmail.com>
2023-10-25 23:04:36 +0700

    Add duration for index Project Btn

--- pages/index.js
+++ pages/index.js
@@ -18,19 +18,40 @@ const Home = () => {
 			<div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
 				<div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
 					{/* title */}
-					<h1 className="h1">
+					<motion.h1
+						variants={fadeIn("down", 0.2)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="h1"
+					>
 						Transforming Ideads <br /> Into{" "}
 						<span className="text-accent">Digital Reality</span>{" "}
-					</h1>
+					</motion.h1>
 					{/* sub titles */}
-					<p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+					<motion.p
+						variants={fadeIn("down", 0.3)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+					>
 						Nostrud excepteur eiusmod anim proident Lorem in. Ex ullamco do ad
 						sunt ea sit aliqua veniam sit irure deserunt.
-					</p>
+					</motion.p>
 					{/* button */}
-					<div className="flex justify-center relative">
+					<div className="flex justify-center xl:hidden relative">
 						<ProjectsBtn />
 					</div>
+					<motion.div
+						variants={fadeIn("down", 0.4)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="hidden xl:flex"
+					>
+						<ProjectsBtn />
+					</motion.div>
 				</div>
 			</div>
 			{/* image */}
