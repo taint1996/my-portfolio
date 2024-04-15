5f43b39d06502a31d53b905a4c243eef3fe7918f
Tai Nguyen-Trung <tai.nguyentrung1996@gmail.com>
2024-04-15 12:29:44 +0700

    Update my About UI

--- components/Socials.js
+++ components/Socials.js
@@ -1,36 +1,54 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 // icons
 import {
-	RiYoutubeLine,
-	RiInstagramLine,
-	RiFacebookBoxLine,
-	RiDribbbleLine,
-	RiBehanceLine,
-	RiPinterestLine,
-} from "react-icons/ri";
+  RiYoutubeLine,
+  RiFacebookBoxLine,
+  RiLinkedinLine,
+  RiGithubLine,
+  RiTiktokLine
+} from 'react-icons/ri';
 
 const Socials = () => {
-	return <div className="flex items-center gap-x-5 text-lg">
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiYoutubeLine/>
-    </Link>
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiFacebookBoxLine/>
-    </Link>
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiInstagramLine/>
-    </Link>
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiDribbbleLine/>
-    </Link>
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiBehanceLine/>
-    </Link>
-    <Link href='/' className="hover:text-accent transition-all duration-300">
-      <RiPinterestLine/>
-    </Link>
-  </div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href="/"
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiYoutubeLine />
+      </Link>
+      <Link
+        href="/"
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiFacebookBoxLine />
+      </Link>
+      <Link
+        href="/"
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiTiktokLine />
+      </Link>
+      <Link
+        href="/"
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+      <Link
+        href="/"
+        target="_blank"
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
